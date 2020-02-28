1429a1990afd6a2b530fa213d910bf3083f24c8b
David Richards <dmricha3@gmail.com>
2020-02-27 18:40:02 -0600

    davidsSecondSolution

---
+++ davidsSecondSolution/davidsSecondProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace davidsSecondProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
